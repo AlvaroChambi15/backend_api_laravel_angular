e71e8f0ed92d76a322958569cfd381ebcf91dcf4
AlvaroMC <alvaromch1510@gmail.com>
2026-03-09 17:04:00 -0400

    Migraciones

---
+++ database/migrations/2026_03_09_201647_create_personas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('personas', function (Blueprint $table) {
+            $table->id();
+            $table->string("nombres", 50);
+            $table->string("apellidos", 50)->nulable();
+            $table->string("foto", 50)->nulable();
+            $table->date("fecha_nacimiento", 50)->nulable();
+            $table->string("direccion", 50)->nulable();
+
+            //Relación con USER
+            $table->bigInteger("user_id")->unsigned();
+            $table->foreign("user_id")->references("id")->on("users");
+
+            $table->timestamps();  //created_at, updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('personas');
+    }
+};
---
+++ database/migrations/2026_03_09_203240_create_productos_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string("nombre", 200);
+            $table->decimal("precio", 10, 2)->nulable();
+
+            $table->string("imagen")->nulable();
+            $table->string("descripcion")->nulable();
+            $table->integer("stock")->nulable();
+            $table->boolean("estado")->default(true);
+
+            //Relación con CATEGORIA
+            $table->bigInteger("categoria_id")->unsigned();
+            $table->foreign("categoria_id")->references("id")->on("categorias");
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('productos');
+    }
+};
